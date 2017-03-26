a98403a85cf1a7fe595f6eeb70b9adb05bbb2487
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2017-03-25 21:04:35 -0600

    fix mess with ids

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from users.views import MessengerBotView, test_food
+from users.views import MessengerBotView, test_food, analytics
 
 urlpatterns = [
     url(r'^messenger/$', MessengerBotView.as_view()),
     url(r'^test/$', test_food),
-    url(r'^users/(?P<fbid>\w{0,50})/$', test_food, name='user_stats'),
+    url(r'^users/(?P<fbid>\w{0,50})/$', analytics, name='user_stats'),
 
 ]
