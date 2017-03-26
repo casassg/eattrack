eca943e58f3348493a8f08fc9b7435d40fbc59d3
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2017-03-26 08:32:04 -0600

    remove from urls

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^test/$', views.test_food),
     url(r'^users/(?P<fbid>\w{0,50})/$', views.analytics, name='user_stats'),
     url(r'^ajax/calories/week.json$', views.LineChartJSONView.as_view(), name='line_chart_json'),
-    url(r'^ajax/product/usage.json$', views.ColumnHighChartJSONView.as_view(), name='column_chart_json'),
 
 ]
